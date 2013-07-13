cad0b71707dfa5929bc18b507ac2f2910105f24e
Ethan Ransom <derethanhausen@gmail.com>
2013-07-13 15:57:00 -0600

    Extract `rank_move` method.

--- lib/smart_player.rb
+++ lib/smart_player.rb
@@ -16,35 +16,7 @@ module TicTacToe
       moves = grid.moves.shuffle!
 
       ranked_moves = moves.collect do |move|
-        rank = 0
-        forecast = grid.clone
-        forecast.apply move, @symbol
-
-        # if the move will let us win, it is very important!
-        rank += 9_000 if forecast.won?
-
-        # more points for more neighbors
-        # Move.new(move).neighbor_coords.each do |coord|
-        #   rank += 10 if grid.at(coord) == @symbol
-        # end
-
-        # corners are worth 20, edges 10
-        rank += ((move[0] - 1).abs + (move[1] - 1).abs) * 10
-        # center is worth 30
-        rank += 30 if move[0] == 1 && move[1] == 1
-
-        # Moves that allow the player to win next round loose lots o' points.
-        # Hopefully this will allow the blocking move to "float" to the top.
-        simulated_other_player = SmartPlayer.new(([:x, :o] - [@symbol]).first) # AIception!
-        simulated_other_player.recursive = true
-        forecast.apply simulated_other_player.move(forecast), simulated_other_player.symbol unless @recursive || forecast.stalemate? # this is crap code. fix API
-        if forecast.won? 
-          rank -= 8_000 # Less than the win points. An immediate win is better than a failure to block.
-          # puts "#{move} would have allowed opponent to win next turn" unless @recursive
-        else
-          # puts "#{move} is okay in regards to short term impacts" unless @recursive
-        end
-
+        rank = rank_move move, grid
 
         {move: move, rank: rank}
       end
@@ -56,6 +28,35 @@ module TicTacToe
 
       # puts "#{@symbol.to_s} moves to #{move.inspect}" unless @recursive
       move
+    end
+  private
+
+    def rank_move move, grid
+      rank = 0
+      forecast = grid.clone
+      forecast.apply move, @symbol
+
+      # if the move will let us win, it is very important!
+      rank += 9_000 if forecast.won?
+
+      # corners are worth 20, edges 10
+      rank += ((move[0] - 1).abs + (move[1] - 1).abs) * 10
+      # center is worth 30
+      rank += 30 if move[0] == 1 && move[1] == 1
+
+      # Moves that allow the player to win next round loose lots o' points.
+      # Hopefully this will allow the blocking move to "float" to the top.
+      simulated_other_player = SmartPlayer.new(([:x, :o] - [@symbol]).first) # AIception!
+      simulated_other_player.recursive = true
+      forecast.apply simulated_other_player.move(forecast), simulated_other_player.symbol unless @recursive || forecast.stalemate? # this is crap code. fix API
+      if forecast.won? 
+        rank -= 8_000 # Less than the win points. An immediate win is better than a failure to block.
+        # puts "#{move} would have allowed opponent to win next turn" unless @recursive
+      else
+        # puts "#{move} is okay in regards to short term impacts" unless @recursive
+      end
+
+      rank
     end
   end
 end
